9b491b8945dfa109578b8d97bc6a88c27b68c064
alex.junior <alex.junior@consulting-for.edenred.com>
2022-12-06 01:09:20 -0300

    Concrete Creator

--- src/main/kotlin/factoryMethod/ConcreteCreator.kt
+++ src/main/kotlin/factoryMethod/ConcreteCreator.kt
@@ -1,5 +1,42 @@
 package factoryMethod
 
-class ConcreteCreator {
+fun main() {
+    val factory = AnimalFactory()
+    factory.getAnimal(Animals.DOG).type()
+    factory.getAnimal(Animals.CAT).type()
+    factory.getAnimal(Animals.HORSE).type()
+}
 
+class AnimalFactory {
+    fun getAnimal(type : Animals) : Animal = when(type) {
+        Animals.DOG -> Dog()
+        Animals.CAT -> Cat()
+        Animals.HORSE -> Horse()
+    }
+}
+
+enum class Animals {
+    DOG, CAT, HORSE
+}
+
+interface Animal {
+    fun type()
+}
+
+class Dog : Animal {
+    override fun type() {
+        println("Canis lupus familiaris")
+    }
+}
+
+class Cat : Animal {
+    override fun type() {
+        println("Felis catus")
+    }
+}
+
+class Horse : Animal {
+    override fun type() {
+        println("Equus caballus")
+    }
 }
