bcba405183b34d7b4b5a6775e53d3204d5a3355a
alex.junior <alex.junior@consulting-for.edenred.com>
2023-01-18 12:31:26 -0300

    Prototype

---
+++ src/main/java/creationPattern/Prototype/PrototypeExample.java
@@ -0,0 +1,40 @@
+package creationPattern.Prototype;
+
+class AppMain {
+    public static void main(String[] args) {
+        Person john = new Person("John", 30, new Address("Main St", "New York"));
+        Person jane = john.clone();
+    }
+}
+
+class Person implements Cloneable {
+    private String name;
+    private int age;
+    private Address address;
+
+    public Person(String name, int age, Address address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public Person clone() {
+        try {
+            return (Person) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+class Address {
+    private String street;
+    private String city;
+
+    public Address(String street, String city) {
+        this.street = street;
+        this.city = city;
+    }
+}
